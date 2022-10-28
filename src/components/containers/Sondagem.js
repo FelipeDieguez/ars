e2132730d45ad18fabcece64a6b0f90d3cbcd093
FELIPE DIEGUEZ <felipedieguez2000@hotmail.com>
2022-10-28 01:06:14 -0300

    initial commit

--- src/components/containers/Sondagem.js
+++ src/components/containers/Sondagem.js
@@ -1,5 +1,4 @@
-import {useState} from 'react'
-
+import { useState } from 'react'
 
 import Button from '../form/Button'
 import Label from '../form/Label'
@@ -12,7 +11,15 @@ import Table from '../form/Table'
 import styles from './Sondagem.module.css'
 
 function Sondagem() {
+    function cadastrar(e) {
+        e.preventDefault()
+        console.log(solo)
+        console.log(nspt)
+    }
+
     const [radio, setRadio] = useState(["Areia", "Areia siltosa", "Areia silto-argilosa", "Areia argilosa", "Areia argilo-siltosa"])
+    const [solo, setSolo] = useState()
+    const [nspt, setNspt] = useState()
 
     const data = [
         {
@@ -35,106 +42,126 @@ function Sondagem() {
 
     return (
         <div className={styles.grid}>
-            <div className={styles.gridSolos}>
-                <div style={{"justifyContent":"flex-start"}}>
-                    <Label text="SONDAGEM:"/>
+            <header>
+                <Label text="SONDAGEM:" />
+                <div className={styles.stepsContainer}>
+                    <div className={styles.steps}>
+                        <div className={styles.step}>
+                            <Radio
+                                text="Areia"
+                                name="solos"
+                                onChange={() => { setRadio(["Areia", "Areia siltosa", "Areia silto-argilosa", "Areia argilosa", "Areia argilo-siltosa"]) }}
+                                id="areia"
+                            />
+                            <Radio
+                                text="Argila"
+                                name="solos"
+                                onChange={(e) => { setRadio(["Argila", "Argila arenosa", "Argila areno-siltosa", "Argila siltosa", "Argila silto-arenosa"]) }}
+                                id="argila"
+                            />
+                            <Radio
+                                text="Silte"
+                                name="solos"
+                                onChange={(e) => { setRadio(["Silte", "Silte arenoso", "Silte areno-argiloso", "Silte argiloso", "Silte argilo-arenoso"]) }}
+                                id="silte"
+                            />
+                        </div>
+                        <div className={styles.step}>
+                            <Select
+                                text="Solo:"
+                                list={radio}
+                                width="160px"
+                                id="solos"
+                                onChange={(e) => setSolo(e.target.value)}
+                            />
+                            <LineEdit
+                                type="number"
+                                text="Nspt="
+                                width="50px"
+                                id="nspt"
+                                onChange={(e) => setNspt(e.target.value)}
+                            />
+                        </div>
+                        <div className={styles.step}>
+                            <Button
+                                text="Cadastrar"
+                                width="100px"
+                                id="cadastrar"
+                                onSubmit={cadastrar}
+                            />
+                            <Button
+                                text="Editar"
+                                width="70px"
+                                id="editar"
+                            />
+                            <Button
+                                text="Remover"
+                                width="100px"
+                                id="remover"
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.steps}>
+                        <div className={styles.step}>
+                            <Label text="MÉTODOS" />
+                        </div>
+                        <div className={styles.step}>
+                            <Radio
+                                text="Aoki-Velloso"
+                                name="metodos"
+                                id="aoki"
+                            />
+                        </div>
+                        <div className={styles.step}>
+                            <Radio
+                                text="Decourt-Quaresma"
+                                name="metodos"
+                                id="decourt"
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.steps}>
+                        <div className={styles.step}>
+                            <Select
+                                text="Tipo:"
+                                list={["Franki", "Metálica", "Pré-moldada", "Escavada", "Raiz", "Hélice contínua", "Barrete", "Ômega"]}
+                                width="135px"
+                                id="tipos"
+                            />
+                        </div>
+                        <div className={styles.step}>
+                            <LineEdit
+                                type="number"
+                                text="Diâmetro="
+                                width="50px"
+                                id="dimensao_1"
+                            />
+                        </div>
+                        <div className={styles.step}>
+                            <Button
+                                text="Calcular"
+                                width="80px"
+                                id="calcular"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <Radio
-                        text="Areia"
-                        name="solos"
-                        onChange={()=>{ setRadio(["Areia", "Areia siltosa", "Areia silto-argilosa", "Areia argilosa", "Areia argilo-siltosa"]) }}
-                        id="areia"
-                    />
-                    <Radio
-                        text="Argila"
-                        name="solos"
-                        onChange={(e)=>{ setRadio(["Argila", "Argila arenosa", "Argila areno-siltosa", "Argila siltosa", "Argila silto-arenosa"]) }}
-                        id="argila"
-                    />
-                    <Radio
-                        text="Silte"
-                        name="solos"
-                        onChange={(e)=>{ setRadio(["Silte", "Silte arenoso", "Silte areno-argiloso", "Silte argiloso", "Silte argilo-arenoso"]) }}
-                        id="silte"
-                    />
-                </div>
-                <div>
-                    <Select
-                        text="Solo:"
-                        list={radio}
-                        width="160px"
-                        id="solos"
-                    />
-                    <LineEdit
-                        type="number"
-                        text="Nspt="
-                        width="50px"
-                        id="nspt"
-                    />
-                </div>
-                <div>
-                    <Button
-                        text="Cadastrar"
-                        width="100px"
-                        id="cadastrar"
-                    />
-                    <Button
-                        text="Editar"
-                        width="70px"
-                        id="editar"
-                    />
-                    <Button
-                        text="Remover"
-                        width="100px"
-                        id="remover"
-                    />
-                </div>
-            </div>
-            <div className={styles.gridMetodos}>
-                <div>
-                    <Label text="MÉTODOS" />
-                </div>
-                <div>
-                    <Radio
-                        text="Aoki-Velloso"
-                        name="metodos"
-                        id="aoki"
-                    />
-                </div>
-                <div>
-                    <Radio
-                        text="Decourt-Quaresma"
-                        name="metodos"
-                        id="decourt"
-                    />
-                </div>
-            </div>
-            <div className={styles.gridDimensoes}>
-                <div>
-                    <Select
-                        text="Tipo:"
-                        list={["Franki", "Metálica", "Pré-moldada", "Escavada", "Raiz", "Hélice contínua", "Barrete", "Ômega"]}
-                        width="135px"
-                        id="tipos"
-                    />
-                </div>
-                <div>
-                    <LineEdit
-                        type="number"
-                        text="Diâmetro="
-                        width="50px"
-                        id="dimensao_1"
-                    />
-                </div>
-                <div>
-                    <Button
-                        text="Calcular"
-                        width="80px"
-                        id="calcular"
-                    />
-                </div>
-            </div>
+            </header>
+            <nav>
+                <Tab 
+                    text="Compressão" 
+                    name="tabelas"
+                    id="compressao"
+                />
+                <Tab 
+                    text="Tração"
+                    name="tabelas"
+                    id="tracao"
+                />
+            </nav>
+            <section>
+                <Table data={data} />
+            </section>
         </div>
     )
 }
--- src/components/form/Button.js
+++ src/components/form/Button.js
@@ -2,11 +2,11 @@ import styles from './Button.module.css'
 
 function Button ({text, width, id}) {
     return (
-        <div>
+        <>
             <button id={id} className={styles.btn} style={{width: width}}>
                 {text}
             </button>
-        </div>
+        </>
     )
 }
 

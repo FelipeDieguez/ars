3c41392d780e3759092f2f3caf8a2c804b1a346d
FelipeDieguez <felipedieguez2000@hotmail.com>
2024-01-14 10:41:20 -0300

    update Aoki parameters

--- src/utils/services/api.js
+++ src/utils/services/api.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export const api = axios.create({
     // baseURL: 'https://fundars.mysticker.io'
-    baseURL: 'http://localhost:3100'
+    // baseURL: 'http://localhost:3100'
+    baseURL: 'http://127.0.0.1:8080'
+    // baseURL: 'https://ars-gxyv3.ondigitalocean.app'
 });
 
 // Intercerpetor de requisições
